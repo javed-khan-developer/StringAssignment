a0472d50aeeaab625ff33879771843456388d343
khanjaved443 <57727397+khanjaved443@users.noreply.github.com>
2023-07-30 15:28:27 +0530

    palindrome string done

---
+++ src/PalindromeSTring.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PalindromeSTring {
+    // 2.Write a java program to know whether the given string is palindrome
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String to check Palindrome");
+        String userInput = sc.nextLine();
+        sc.close();
+        String reversedString = "";
+        int length = userInput.length() - 1;
+        for (int i = length; i >= 0; i--) {
+            reversedString += userInput.charAt(i);
+        }
+        if (userInput.equals(reversedString)) {
+            System.out.println("it is a plaindrome string");
+        } else {
+            System.out.println("it is not a plaindrome string");
+
+        }
+    }
+}
