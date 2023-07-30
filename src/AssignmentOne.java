77f6d78a2faeb0fec08fcb985e0650339caf7f66
khanjaved443 <57727397+khanjaved443@users.noreply.github.com>
2023-07-30 15:06:38 +0530

    reverse string completed

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -1,5 +1,19 @@
-public class App {
+public class AssignmentOne {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // 1.Write a java program to Reverse a string without using the inbuilt method.
+        String string = "javed";
+        int i = 0;
+        int j = string.length() - 1;
+        char[] charArray = string.toCharArray();
+
+        while (i < j) {
+            char temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            i++;
+            j--;
+        }
+        String reversedString = new String(charArray);
+        System.out.println(reversedString);
     }
 }
